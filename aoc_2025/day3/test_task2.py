ba3cacbddb0fe8ff14fd4a48756cbfa3f6d30ba4
Luka Šulc <68392977+lukasulc@users.noreply.github.com>
2025-12-15 19:21:45 +0000

    Add tests for task 2

---
+++ aoc_2025/day3/test_task2.py
@@ -0,0 +1,43 @@
+import unittest
+from task2 import get_largest_joltage
+
+class TestTask1(unittest.TestCase):
+    def test_dummy_example(self):
+        banks = [
+            "821111111119",
+        ]
+        solutions = [
+            821111111119,
+        ]
+        for bank, solution in zip(banks, solutions):
+            self.assertEqual(get_largest_joltage(bank), solution)
+
+    def test_last_numbers_example(self):
+        banks = [
+            "8121111111119",
+        ]
+        solutions = [
+            821111111119,
+        ]
+        for bank, solution in zip(banks, solutions):
+            self.assertEqual(get_largest_joltage(bank), solution)
+
+    def test_offical_example(self):
+        banks = [
+            "987654321111111",
+            "811111111111119",
+            "234234234234278",
+            "818181911112111",
+        ]
+        solutions = [
+            987654321111,
+            811111111119,
+            434234234278,
+            888911112111,
+        ]
+        for bank, solution in zip(banks, solutions):
+            self.assertEqual(get_largest_joltage(bank), solution)
+
+
+if __name__ == "__main__":
+    unittest.main()
