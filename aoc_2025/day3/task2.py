152560c34687c217fd342a61ded7a2f1c2c3de1d
Luka Šulc <68392977+lukasulc@users.noreply.github.com>
2025-12-15 19:22:27 +0000

    Add part of solution for task 2

---
+++ aoc_2025/day3/task2.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+
+def get_largest_joltage(large_num_string: str, num_batteries: int = 12) -> int:
+    max_joltage = large_num_string
+
+    for i in range(len(large_num_string) - num_batteries):
+        for battery_idx in range(num_batteries):
+            if int(max_joltage[battery_idx]) <= int(large_num_string[i]):
+                reset_batteries = num_batteries - battery_idx
+                max_joltage = max_joltage[:battery_idx] + large_num_string[i:i+reset_batteries]
+                break
+        
+    
+
+    return int(max_joltage) 
+
+def main(input_path: Path):
+    banks = []
+    with input_path.open("r", encoding="utf-8") as f:
+        banks = [line.rstrip() for line in f]
+
+    joltage = 0
+    for bank in banks:
+        joltage += get_largest_joltage(bank)
+
+    print(joltage)
+
+if __name__ == "__main__":
+    # Open input relative to this script's directory so the script works
+    # no matter what the current working directory is when invoked.
+    script_dir = Path(__file__).parent
+    input_path = script_dir / "input.txt"
+
+    main(input_path)
